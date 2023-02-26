9aa1ade28e04886041e0fbbdc3b8fd7b40f5461c
Zhanagul <190107044@stu.sdu.edu.kz>
2023-02-26 13:28:22 +0600

    Initial Commit

---
+++ OrynTap/OrynTapApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrynTapApp.swift
+//  OrynTap
+//
+//  Created by Zhanagul on 26.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct OrynTapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
